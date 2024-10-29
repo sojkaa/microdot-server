51d976a54fb0170ba32ceea7ea5ce7e6a64ff0ea
Santiago Sojka <80698096+sojkaa@users.noreply.github.com>
2024-10-29 09:21:29 -0300

    Update boot.py

--- actividades/ej_02/boot.py
+++ actividades/ej_02/boot.py
@@ -1,1 +1,15 @@
-# Configuracion inicial
+import network
+from time import sleep
+
+def do_connect():
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print("Connecting to network...")
+        sta_if.active(True)
+        sta_if.connect("Cooperadora Alumnos", "")
+        while not sta_if.isconnected():
+            print(".", end="")
+            sleep(0.25)  
+    print("Network config:", sta_if.ifconfig())
+
+do_connect()
